626a3f91143c7ebb40069262716352259f5252e0
Konstantin Tumashevich <70195045+Alvaisari@users.noreply.github.com>
2025-02-09 11:29:27 +0200

    Init REST API in Go project

---
+++ Day 2. Build a REST API in Go/main.go
@@ -0,0 +1,17 @@
+package main
+
+// game represents data about a record game
+type game struct {
+	ID string `json:"id"`
+	Title string `json:"title"`
+	Developer string `json:"developer"`
+	Price string `json:"price"`
+}
+
+// Slice of games to fill in the data
+var games = []game{
+	{ID: "1", Title: "Mass Effect", Developer: "BioWare", Price: "50"},
+	{ID: "2", Title: "Fallout", Developer: "Obsidian Entertainment", Price: "30"},
+	{ID: "3", Title: "Outer Wilds", Developer: "Mobius Digital", Price: "20"},
+	{ID: "4", Title: "Civilization VI", Developer: "Firaxis Games", Price: "60"},
+}
