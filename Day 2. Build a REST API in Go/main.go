20aba601ec5556d52f7b2f54dbb931774581369e
Konstantin Tumashevich <70195045+Alvaisari@users.noreply.github.com>
2025-02-15 23:07:00 +0200

    Fix price type to float64

--- Day 2. Build a REST API in Go/main.go
+++ Day 2. Build a REST API in Go/main.go
@@ -11,15 +11,15 @@ type game struct {
 	ID string `json:"id"`
 	Title string `json:"title"`
 	Developer string `json:"developer"`
-	Price string `json:"price"`
+	Price float64 `json:"price"`
 }
 
 // Slice of games to fill in the data
 var games = []game{
-	{ID: "1", Title: "Mass Effect", Developer: "BioWare", Price: "50"},
-	{ID: "2", Title: "Fallout", Developer: "Obsidian Entertainment", Price: "30"},
-	{ID: "3", Title: "Outer Wilds", Developer: "Mobius Digital", Price: "20"},
-	{ID: "4", Title: "Civilization VI", Developer: "Firaxis Games", Price: "60"},
+	{ID: "1", Title: "Mass Effect", Developer: "BioWare", Price: 50},
+	{ID: "2", Title: "Fallout", Developer: "Obsidian Entertainment", Price: 30},
+	{ID: "3", Title: "Outer Wilds", Developer: "Mobius Digital", Price: 20},
+	{ID: "4", Title: "Civilization VI", Developer: "Firaxis Games", Price: 60},
 }
 
 // Get all games as JSON
